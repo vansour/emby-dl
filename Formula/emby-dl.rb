0bf24e7375ee694864a7bb5f0e07ba3a3a997c6f
vansour <vansoursheng@outlook.com>
2026-07-01 08:05:01 +0800

    fix: update bucket & formula hashes for v0.0.7 release

--- Formula/emby-dl.rb
+++ Formula/emby-dl.rb
@@ -7,11 +7,11 @@ class EmbyDl < Formula
   on_macos do
     on_intel do
       url "https://github.com/vansour/emby-dl/releases/download/v#{version}/emby-dl-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "89cf53748943c4b9daf9f61adfcdcb3ac1e2a06415087c3c2edf9ab68e95b8dc"
     end
     on_arm do
       url "https://github.com/vansour/emby-dl/releases/download/v#{version}/emby-dl-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "e4036b02e823e7941a59af5f30278338f0b8899264fa0eec66ffbcaf2045f78d"
     end
   end
 
